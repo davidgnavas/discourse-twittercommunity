70a7961b56060d071d3e22bf6f563fb3ae7a0fc2
David García-Navas López de Cuéllar <davidgarcianavas@gmail.com>
2015-01-09 22:02:39 +0100

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,2 +1,2 @@
 register_asset "javascripts/discourse/templates/connectors/poster-avatar-bottom/under-avatar.js.handlebars"
-register_asset "stylesheets/twittercommunity.scss"
+register_asset "stylesheets/lidertarios.scss"
